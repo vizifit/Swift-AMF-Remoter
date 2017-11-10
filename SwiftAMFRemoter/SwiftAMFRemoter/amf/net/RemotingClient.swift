3a82d0439d46f7b300be49e2df33b11dc4db30d0
Tony Henderson <info@vizifit.com>
2017-11-10 03:06:23 -0800

    Fixed issue with timing of send message

--- SwiftAMFRemoter/SwiftAMFRemoter/amf/net/RemotingClient.swift
+++ SwiftAMFRemoter/SwiftAMFRemoter/amf/net/RemotingClient.swift
@@ -170,9 +170,9 @@ open class RemotingClient: INetConnection {
                 
                 // (1) HAS NO PENDING message results
                 if(self._pendingMessageCount == 1 && self._pendingMessagesRaw.count == 0){
-                    self.sendMessage(rawMessage: resultMessage as! [UInt8])
                     self._pendingMessageCount = 0 // Reset pending message(s) count
-                    self._isPendingMessageResponse = false
+                    self._isPendingMessageResponse = false 
+                    self.sendMessage(rawMessage: resultMessage as! [UInt8])
                     self._pendingMessagesRaw.removeAll()
                     return
                 }
